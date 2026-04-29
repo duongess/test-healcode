11ebfc48678ab598c58dd8012527d40f9e8bfc6c
duongess <duongcantat@gmail.com>
2026-04-29 14:22:22 +0700

    error

--- java/utils/TestJava.java
+++ java/utils/TestJava.java
@@ -1,9 +1,9 @@
 package utils;
 import java.util.ArrayList;
-import utils.TestJava01;
+import utils.TestJava01
 // Tên file: TestJava.java
 public class TestJava {
-    public static void main(String[] args) {
+    public static void main(String[] args) 
         String greeting = "Hello";
         System.out.println(greeting)
         TestJava01.test("Hi");
